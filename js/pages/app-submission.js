a0f0368b79b71faabcb2969e1b7544e40d30a39d
lkwebdev <lkwebdev@mail.ru>
2022-04-07 18:18:22 +0300

    ~

--- js/pages/app-submission.js
+++ js/pages/app-submission.js
@@ -84,4 +84,133 @@ document.addEventListener('DOMContentLoaded', () => {
         onSelect(item) {
         }
     });
+
+
+    // Sortable
+    let rows = document.querySelectorAll('.photo-gallery__images .sortableContainer');
+    for (let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+
+        let sort = new Sortable(row, {
+            swapThreshold: 1,
+            animation: 150,
+            ghostClass: 'blue-background-class',
+            onUpdate: (e) => {
+            }
+        });
+    }
+
+    // Dropzone
+    let photoGalleryDropzones = document.querySelectorAll('.dropzoneContainer');
+
+    photoGalleryDropzones.forEach((el, index) => {
+        let dropzone = photoGalleryDropzones[index];
+        let dropzoneInput = dropzone.querySelector('.dropzoneInput');
+        const inputMultiple = false;
+
+        ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function(event) {
+            dropzone.addEventListener(event, function(e) {
+                e.preventDefault();
+                e.stopPropagation();
+            });
+
+            dropzone.addEventListener('dragover', (e) => {
+                dropzone.classList.add('dropzone-dragging');
+            }, false);
+            dropzone.addEventListener('dragleave', (e) => {
+                dropzone.classList.remove('dropzone-dragging');
+            }, false);
+            dropzone.addEventListener('click', function(e) {
+                dropzoneInput.click();
+            });
+
+            dropzone.addEventListener('drop', (e) => {
+                dropzone.classList.remove('dropzone-dragging');
+                let files = e.dataTransfer.files;
+                let dataTransfer = new DataTransfer();
+
+                if (!inputMultiple) {
+                    let file = files[0];
+                    dataTransfer.items.add(file);
+
+                    let reader = new FileReader();
+
+                    reader.onload = function (e) {
+                        let img = dropzone.querySelector('img');
+                        if (img && img.length !== 0) {
+                            img.remove();
+                        }
+                        let loadBtn = dropzone.querySelector('.photo-galley__load-btn');
+                        if (loadBtn && loadBtn.length !== 0) {
+                            loadBtn.remove();
+                        }
+
+                        let node = document.createElement('img');
+                        node.setAttribute('src', e.target.result);
+                        dropzone.appendChild(node);
+                    };
+
+                    reader.readAsDataURL(file);
+                }
+
+                let filesToBeAdded = dataTransfer.files;
+                dropzoneInput.files = filesToBeAdded;
+
+            }, false);
+        });
+
+    });
+
+
+    /*
+    let dropzone = document.querySelector('.dropzone');
+    console.log(dropzone);
+    let dropzoneInput = dropzone.querySelector('.dropzone-input');
+    console.log(dropzoneInput);
+    let multiple = dropzoneInput.getAttribute('multiple') ? true : false;
+    console.log(multiple);
+
+    ['drag', 'dragstart', 'dragend', 'dragover', 'dragenter', 'dragleave', 'drop'].forEach( function(event) {
+        dropzone.addEventListener(event, function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+    });
+
+    dropzone.addEventListener('dragover', (e) => {
+        dropzone.classList.add('dropzone-dragging');
+    }, false);
+    dropzone.addEventListener('dragleave', (e) => {
+        dropzone.classList.remove('dropzone-dragging');
+    }, false);
+
+    dropzone.addEventListener('drop', (e) => {
+        dropzone.classList.remove('dropzone-dragging');
+        let files = e.dataTransfer.files;
+        let dataTransfer = new DataTransfer();
+
+        if (!multiple) {
+            let file = files[0];
+            dataTransfer.items.add(file);
+
+            let reader = new FileReader();
+
+            reader.onload = function (e) {
+                let node = document.createElement('img');
+                node.setAttribute('src', e.target.result);
+                dropzone.appendChild(node);
+            };
+
+            reader.readAsDataURL(file);
+        }
+
+        let filesToBeAdded = dataTransfer.files;
+        dropzoneInput.files = filesToBeAdded;
+
+    }, false);
+
+    dropzone.addEventListener('click', function(e) {
+        dropzoneInput.click();
+    });
+    */
 });
